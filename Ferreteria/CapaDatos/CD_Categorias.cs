95970faf86d38b9a6d6021fb0f256ca53c8792c1
jlsoza <67883680+jlsoza@users.noreply.github.com>
2020-09-01 10:52:21 -0600

    categorias

---
+++ Ferreteria/CapaDatos/CD_Categorias.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class CD_Categorias
+    {
+        private ConexionBD conexion = new ConexionBD();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Sp_MostrarCategorias";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+        }
+
+        public DataTable ListaDeCategorias(string filtro)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Sp_listadoCategorias";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.Clear();
+            comando.Parameters.AddWithValue("@filtro", filtro);
+
+            leer = comando.ExecuteReader();
+            tabla.Clear();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+        }
+
+        public void InsertarCategoria(string Codigo_Cat, string Nombre_Cat, string Descripcion_Cat, byte Estado_Categoria)
+        {
+            //PROCEDIMIENTO
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Sp_InsertarCategoria";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@Codigo_Cat", Codigo_Cat);
+            comando.Parameters.AddWithValue("@Nombre_Cat", Nombre_Cat);
+            comando.Parameters.AddWithValue("@Descripcion_Cat", Descripcion_Cat);
+            comando.Parameters.AddWithValue("@Estado_Categoria", Estado_Categoria);
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+    }
+
+
+}
