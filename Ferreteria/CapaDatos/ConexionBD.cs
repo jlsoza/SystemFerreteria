6628cf2797e37ae0bf4be513916981d51783de38
jlsoza <67883680+jlsoza@users.noreply.github.com>
2020-09-01 10:35:17 -0600

    conexion

---
+++ Ferreteria/CapaDatos/ConexionBD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    class ConexionBD
+    {
+        private SqlConnection Conexion = new SqlConnection(@"Server = DESKTOP-OBLOVHB\SQLEXPRESS; DataBase = BDFerreteria; Integrated Security = true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+
+    }
+}
